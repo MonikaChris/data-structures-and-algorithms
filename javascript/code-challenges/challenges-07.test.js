dfd4417fa0fae086bfeb00c287193a1ec4cf4b84
Monika <monidavies@gmail.com>
2022-08-05 08:14:04 -0700

    Complete Problem 9

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -179,7 +179,12 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  // Solution code here...
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr.splice(i,1);
+      i--;
+    }
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -309,7 +314,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);

bdcb9d0fef71b9b67fe453505d0cf6f0d51e6c50
Monika <monidavies@gmail.com>
2022-08-23 08:37:09 -0700

    Complete Problem 5

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -63,7 +63,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  return /^\w+.\w+@[a-z]*(.net|.com|.org)$/g.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
